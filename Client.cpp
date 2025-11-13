489e34cccd7f89010e80ad58cbf948c7f2264be6
David Chang <230165267+justd4vid@users.noreply.github.com>
2025-11-12 22:11:35 -0600

    Full bidirectional comms working

--- Client.cpp
+++ Client.cpp
@@ -1,5 +1,41 @@
+#include <iostream>
+#include <cstring>
+
+#include <sys/socket.h>
+#include<netinet/ip.h>
+#include <unistd.h>
+
+static void die(const char *msg) {
+    std::cerr << msg << ": " << strerror(errno) << "\n";
+    std::exit(EXIT_FAILURE);
+}
+
 int main() {
     std::cout << "Running Client." << "\n";
 
-    
+    int fd = socket(AF_INET, SOCK_STREAM, 0);                       // 1. Obtain handle
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));    // 1.5. Set options
+
+    struct sockaddr_in addr = {};                                   // 3. Connect. Socket is created. 
+    addr.sin_family = AF_INET;          // address family IPv4
+    addr.sin_port = htons(1234);        // port 1234
+    addr.sin_addr.s_addr = htonl(0);    // wildcard- accept connections anywhere
+
+    int rv = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (rv) die("connect()");
+
+    // Write to our socket
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
+    // Read from our socket
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    if(n < 0) die("read()");
+
+    printf("Server says: %s\n", rbuf);
+
+    close(fd);
+    return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -2,48 +2,68 @@
 #include <cstring>      // for strerror()
 
 #include <sys/socket.h> // for socket creation
-#include <netinet/ip.h> 
-
+#include <netinet/ip.h> // for internal protocol addresses
 #include <unistd.h>     // for read(), write(), close()
 
 // Helper to print error and exit. 
-void die(const char *msg) {
+static void die(const char *msg) {
     std::cerr << msg << ": " << strerror(errno) << "\n";
     std::exit(EXIT_FAILURE);
 }
 
+// Helper to print message. 
+static void msg(const char *msg) {
+    std::cout << msg << "\n";
+}
+
+static void dummy_process(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1); // read rbuf size chars!
+    if(n==0) {
+        msg("Client closed connection.");
+        return;
+    }else if(n<0) {
+        msg("read() error");
+        return;
+    }
+    printf("client says: %s\n", rbuf);
+
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+}
+
 int main() {
-    std::cout << "Running Program: " << "\n";
+    std::cout << "Running Server: " << "\n";
+    
     int fd = socket(AF_INET, SOCK_STREAM, 0);                       // 1. Obtain a socket handle. We choose IPV4, TCP
 
-    int val = 1;                                                    // 2. Set socket options. Generally good to set SO_REUSEADDR
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));    //    to allow server to use same IP:port after restart. 
+    int val = 1;                                                    // 1.5. Set socket options. Generally good to set SO_REUSEADDR
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));    //    to allow server to use same IP:port immediately after restart. 
 
-    struct sockaddr_in addr = {};                                   // 3. Bind to an address. We use wildcard 0.0.0.0:1234
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(1234);        // port
-    addr.sin_addr.s_addr = htonl(0);    // wildcard IP 0.0.0.0
+    struct sockaddr_in addr = {};                                   // 2. Bind to an address. We use wildcard 0.0.0.0:1234
+    addr.sin_family = AF_INET;          // address family IPv4
+    addr.sin_port = htons(1234);        // port 1234
+    addr.sin_addr.s_addr = htonl(0);    // wildcard- accept connections anywhere
     int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-    if (rv) {die("bind()");}
+    if (rv) {die("bind()");} // rv: 0 = success, -1 = error
 
 
-    rv = listen(fd, SOMAXCONN);                                     // 4. Listen. Socket is created on listen(). SOMAXCONN is the size of 
-    if (rv) {die("listen()");}                                      //    queue for max number of pending connections. It's 4096 on linux.
+    rv = listen(fd, SOMAXCONN);                                     // 3. Listen. Socket is created on listen(). SOMAXCONN is the 
+    if (rv) die("listen()");                                      //    maximum queue size for pending connections. It's 4096 on linux.
                                                                     //    For modern use-case it has no affect, the queue never fills up.  
 
-    while (true) {                                                  // 5. Accept connections. The server enters loop and processes each
+    while (true) {                                                  // 4. Accept connections. The server enters loop and processes each
         struct sockaddr_in client_addr = {};                        //    client connection. 
         socklen_t addrlen = sizeof(client_addr);
-        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-        if(connfd < 0) {
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen); // when client connects, return a new fd for that specific connection. 
+        if(connfd < 0) {                                                    // The original fd keeps listening. 
             continue; // error
         }
+
+        dummy_process(connfd);
+
         close(connfd);
     }
-
-
-
-
     
     return 0;
 }
